60ae6998656cb19de9b8c9bd884575a480fdd595
pedrocampos <engpedrocampos@gmail.com>
2025-01-03 16:03:57 -0300

    refactor: run pipeline

--- src/main/java/com/example/demo/LiquibaseLambdaHandler.java
+++ src/main/java/com/example/demo/LiquibaseLambdaHandler.java
@@ -2,18 +2,18 @@ package com.example.demo;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-public class LiquibaseLambdaHandler implements RequestHandler<String, String> {
+public class LiquibaseLambdaHandler implements RequestHandler<Object, String> {
 
-    private final AnnotationConfigApplicationContext context;
-
-    public LiquibaseLambdaHandler() {
-        context = new AnnotationConfigApplicationContext(LiquibaseDemoApplication.class);
-    }
+    private ConfigurableApplicationContext context;
 
     @Override
-    public String handleRequest(String input, Context context) {
-        return "Hello, " + input + "!";
+    public String handleRequest(Object input, Context lambdaContext) {
+        if (context == null) {
+            context = SpringApplication.run(LiquibaseDemoApplication.class);
+        }
+        return "Liquibase migration executed successfully!";
     }
 }
